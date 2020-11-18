c1fbe3632a842e39c71529e11335ff51a71de0dc
jwiggs <destiny.jonathan@gmail.com>
2020-11-18 15:45:25 +0800

    initial commit

---
+++ app/src/main/java/com/stuff/tooltrack/MainActivity.java
@@ -0,0 +1,65 @@
+package com.stuff.tooltrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("test/value");
+
+    EditText editTextValue;
+    Button buttonPush;
+    TextView textViewDisplay;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextValue = findViewById(R.id.editTextValue);
+        buttonPush = findViewById(R.id.buttonPush);
+        textViewDisplay = findViewById(R.id.textViewDisplay);
+
+
+        buttonPush.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.setValue(editTextValue.getText().toString());
+            }
+        });
+
+        ValueEventListener valueListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                textViewDisplay.setText("Value: "+dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //do stuff like log errors or what not
+            }
+        };
+        myRef.addValueEventListener(valueListener);
+
+
+    }
+
+
+
+
+
+
+}
