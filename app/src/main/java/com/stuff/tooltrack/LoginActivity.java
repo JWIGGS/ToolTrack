9766fd1b48b219b92ce9c4a887bc8f34c8efa222
jwiggs <destiny.jonathan@gmail.com>
2020-11-19 14:45:21 +0800

    prettier login activity

--- app/src/main/java/com/stuff/tooltrack/LoginActivity.java
+++ app/src/main/java/com/stuff/tooltrack/LoginActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText editTextStudentID;
     EditText editTextStudentEmail;
     Button buttonLogin;
+    TextView textViewError;
 
 
     @Override
@@ -23,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
         editTextStudentID = findViewById(R.id.editTextStudentID);
         editTextStudentEmail = findViewById(R.id.editTextStudentEmail);
         buttonLogin = findViewById(R.id.buttonLogin);
+        textViewError = findViewById(R.id.textViewError);
 
     }
 
