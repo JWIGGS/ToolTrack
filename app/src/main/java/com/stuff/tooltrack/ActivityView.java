b60ccf77125ae4c6fe7686bc0c463c0fa1f4190e
jwiggs <destiny.jonathan@gmail.com>
2020-11-25 16:52:30 +0800

    layout updates based on firebase

--- app/src/main/java/com/stuff/tooltrack/ActivityView.java
+++ app/src/main/java/com/stuff/tooltrack/ActivityView.java
@@ -17,6 +17,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ActivityView extends AppCompatActivity {
 
     LinearLayout linearLayout;
@@ -25,12 +28,14 @@ public class ActivityView extends AppCompatActivity {
     DatabaseReference refTools = database.getReference().child("tools");
     DataSnapshot currentDatabase;
 
-    private boolean initialized = false;
+    HashMap<String, View> layoutMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+
+        layoutMap = new HashMap<String, View>();
 
         linearLayout = findViewById(R.id.linearLayoutView);
 
@@ -53,45 +58,43 @@ public class ActivityView extends AppCompatActivity {
     }
 
 
-    //update the layouts and initialize them if it doesnt have any
+    //initialize and update the layouts
     public void updateLayout(){
 
-        if(!initialized){
-            initialized = true;
+        for(DataSnapshot tool: currentDatabase.getChildren()){
 
-            for(DataSnapshot tool: currentDatabase.getChildren()){
+            String key = tool.getKey();
 
-                View addLayout = LayoutInflater.from(this).inflate(R.layout.item_view, null);
+            View editLayout;
 
-                TextView textViewName = addLayout.findViewById(R.id.textViewName);
-                textViewName.setText(tool.child("name").getValue().toString());
-
-                boolean available = tool.child("available").getValue().toString()=="true";
-                TextView textViewStatus = addLayout.findViewById(R.id.textViewStatus);
-                textViewStatus.setText("Status: "+(available?"available": "not available"));
-                ImageView imageViewStatus = addLayout.findViewById(R.id.imageViewStatus);
-                imageViewStatus.setColorFilter(available? R.color.teal_200: R.color.design_default_color_error);
-                //the colors do not display properly
-
-                TextView textViewLocation = addLayout.findViewById(R.id.textViewLocation);
-                textViewLocation.setText("Location: "+tool.child("rack").getValue().toString());
-
-
-                linearLayout.addView(addLayout);
-
+            if(layoutMap.containsKey(key)) {
+                editLayout = layoutMap.get(key);
             }
+            else{
+                editLayout = LayoutInflater.from(this).inflate(R.layout.item_view, null);
+                layoutMap.put(tool.getKey(), editLayout);
+
+                linearLayout.addView(editLayout);
+            }
+
+            TextView textViewName = editLayout.findViewById(R.id.textViewName);;
+            TextView textViewStatus = editLayout.findViewById(R.id.textViewStatus);
+            TextView textViewLocation = editLayout.findViewById(R.id.textViewLocation);
+            ImageView imageViewStatus = editLayout.findViewById(R.id.imageViewStatus);
+
+
+            textViewName.setText(tool.child("name").getValue().toString());
+            //textViewName.setText(tool.getKey());
+
+            boolean available = tool.child("available").getValue().toString()=="true";
+            textViewStatus.setText("Status: "+(available?"available": "not available"));
+            imageViewStatus.setColorFilter(available? R.color.teal_200: R.color.design_default_color_error);
+
+            textViewLocation.setText("Location: "+tool.child("rack").getValue().toString());
+
+
         }
-        else{
-            //ummm im tired so ill figure this out later
-            //also i think you need to save the names of the children and the id's
-            // accosiated with the layouts you just created so that you can propely
-            // reference the layouts you want to edit later on
-            //also i think you are going to need to do
-            // for(){
-            // if(the element exists){create element}
-            // update element
-            // }
-        }
+
 
 
     }
